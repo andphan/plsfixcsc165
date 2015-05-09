4be5935f6899be4b2cfaa74be0f84432a32e4c9f
andphan <amannamedphan@gmail.com>
2015-05-09 01:41:45 -0700

    wtf

--- src/a3/kmap165Engine/npc/NPC.java
+++ src/a3/kmap165Engine/npc/NPC.java
@@ -1,42 +1,59 @@
 package a3.kmap165Engine.npc;
 
+import graphicslib3D.Matrix3D;
 import graphicslib3D.Point3D;
 
 public class NPC {
+	
 	Point3D location;
-
-	public double getX() {
+	private Matrix3D localTranslation;
+	
+	public NPC()
+	{
+		this.localTranslation = new Matrix3D();
+	}
+	
+	
+	public double getX()
+	{
 		return location.getX();
 	}
-
-	public double getY() {
+	public double getY()
+	{
 		return location.getY();
 	}
-
-	public double getZ() {
+	public double getZ()
+	{
 		return location.getZ();
 	}
-
-	public void mopeAround() {
-		// where i mope
+	
+	public void updateLocation()
+	{
+		location.setX(this.getX());
+		location.setY(this.getY());
+		location.setZ(this.getZ());
+		
 	}
+	public void mopeAround()
+	{
+		System.out.println("moping around");
+	}
+	public void throwPowerUps()
+	{
+		System.out.println("powerups throwing");
+	}
+	public void randomizeLocation(int nextInt, int nextInt2) {
+		this.location.setX(nextInt);
+		this.location.setZ(nextInt2);
+		
+	}
+	public void setLocalTranslation(Matrix3D trans) {
+		localTranslation = trans;
+	}
+
 
 	public void attackAvatar() {
-		// where I COPE
+		System.out.println("attacking avatar");
+		
 	}
-
-	public void randomizeLocation() {
-
-	}
-
-	public void updateLocation() {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void randomizeLocation(int nextInt, int nextInt2) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
